4e1803a347f0f978c586e328a8a2b33c6b63a4d7
EvheniyBystrov <evheniy.bystrov@gmail.com>
2018-12-17 10:27:00 +0200

    init

---
+++ packages/react-compose/README.md
@@ -0,0 +1,3 @@
+# React HOCs Compose
+
+Composing React HOCs
---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    'postcss-cssnext': {
+      warnForDuplicates: false,
+    },
+    cssnano: {
+      zindex: false,
+    },
+  },
+};
