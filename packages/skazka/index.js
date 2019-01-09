42f04f1a02ac281ef6d0d64f310d32ca3d3c20ad
EvheniyBystrov <evheniy.bystrov@gmail.com>
2019-01-09 18:30:35 +0200

    site & release

--- packages/skazka/index.js
+++ packages/skazka/index.js
@@ -1,4 +1,4 @@
-if (process.env.npm_package_author_name !== 'SkazkaJS') {
+if (process.env.npm_package_author_name !== 'skazkajs') {
   console.error('This package is deprecated. See: https://skazkajs.org/'); // eslint-disable-line
   process.exit(1);
 }
