5dfdfed621a4a8b406f8a86240230faac86105dd
evheniy <evheniy.bystrov@gmail.com>
2020-04-22 11:18:42 +0300

    tests

--- packages/aws/handler/timeout.js
+++ packages/aws/handler/timeout.js
@@ -1,5 +1,3 @@
-const pause = require('promise-pause-timeout');
-
 const factory = require('./factory');
 
 const defaultErrorHandler = require('../error/defaultErrorHandler');
@@ -11,10 +9,26 @@ const timeout = factory(async (handler, options = {}, args) => {
   } = options;
 
   try {
+    let id;
+
     return await Promise.race([
-      pause(seconds * 1000).then(() => Promise.reject(new Error(`Timeout: ${seconds} seconds!`))),
+      new Promise((resolve, reject) => {
+        id = setTimeout(() => {
+          clearTimeout(id);
+
+          reject(new Error(`Timeout: ${seconds} seconds!`));
+        }, seconds * 1000);
+      }),
       handler(...args),
-    ]);
+    ]).then((result) => {
+      clearTimeout(id);
+
+      return result;
+    }).catch((error) => {
+      clearTimeout(id);
+
+      return Promise.reject(error);
+    });
   } catch (error) {
     await errorHandler(error, args);
 
